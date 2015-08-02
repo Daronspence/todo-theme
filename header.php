cd432950f6ebf4893dbd5db3165c86419b93dbb1
Daron <daronspence@gmail.com>
2015-08-02 00:44:06 -0500

    theme-color address bar

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#00B16A">
 <meta charset="UTF-8">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
